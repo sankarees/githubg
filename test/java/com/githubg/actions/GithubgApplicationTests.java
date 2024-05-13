00df9dee2a946a66f6854a0f25e31336b12d75ed
Sankareeswaran A <92990298+sankarees@users.noreply.github.com>
2024-05-13 21:40:31 +0530

    PR_create
    PR_create

---
+++ test/java/com/githubg/actions/GithubgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.githubg.actions;
+
+/*import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+*/
+//@SpringBootTest
+class GithubgApplicationTests {
+
+//	@Test
+	void contextLoads() {
+	}
+
+}
